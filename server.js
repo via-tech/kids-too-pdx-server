171f4e0bfc61175d4a3eddb6c99a55f05ae77ac4
Dyanna Tolman <dyannast@gmail.com>
2019-03-22 11:55:57 -0700

    scaffold

---
+++ server.js
@@ -0,0 +1,11 @@
+
+require('dotenv').config();
+require('./lib/utils/connect')();
+
+const app = require('./lib/app');
+
+const PORT = process.env.PORT || 7891;
+
+app.listen(PORT, () => {
+  console.log('RUNNING');
+});
