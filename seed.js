8781c198370138e41500bed3ed5f09a91f8828eb
Teonna Zaragoza <43023749+tmzaragoza@users.noreply.github.com>
2019-03-26 11:28:34 -0700

    Datascraping (#12)
    * added server and app
    
    * testing the scraper
    
    * datascraper
    
    * OMG I can get rid of all of the line breaks!
    
    * scraping
    
    * pdxparent event data scraper
    
    * days working
    
    * deleted datascraper. Everything is in individual files
    
    * passing travis
    
    * working on data

--- seed.js
+++ seed.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 require('./lib/utils/connect')();
 const mongoose = require('mongoose');
 // const seedData = require('./tests/seedData');
+const art = require('./lib/utils/artsParser');
 const data = require('./lib/utils/csvParser');
 
+art({})
+  .then(() => console.log('Done'))
+  .catch(err=> console.error(err))
 
 data ({})
   .then(() => console.log('done'))
@@ -13,5 +17,4 @@ data ({})
 // seedData(100)
 //   .then(() => console.log('done'))
 //   .catch(err => console.error(err))
-// .finally(() => mongoose.connection.close());
-
+//   .finally(() => mongoose.connection.close());
