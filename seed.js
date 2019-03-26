0f09885625fa8195f36d531b9c29f1ccaaad10f5
Teonna Zaragoza <43023749+tmzaragoza@users.noreply.github.com>
2019-03-26 15:24:11 -0700

    Datascraping (#14)
    * added server and app
    
    * testing the scraper
    
    * datascraper
    
    * OMG I can get rid of all of the line breaks!
    
    * scraping
    
    * pdxparent event data scraper
    
    * days working
    
    * deleted datascraper. Everything is in individual files
    
    * passing travis
    
    * working on data
    
    * updated seed file. can run all seed data

--- seed.js
+++ seed.js
@@ -1,20 +1,27 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 const mongoose = require('mongoose');
-// const seedData = require('./tests/seedData');
 const data = require('./lib/utils/csvParser');
 const art = require('./lib/utils/artsParser');
+const library = require('./lib/services/libraryScraper');
+const pdxparent = require('./lib/services/pdxParent');
 
 art({})
   .then(() => console.log('Done'))
+  .catch(err => console.error(err))
   .catch(err=> console.error(err))
 
-data ({})
+data({})
   .then(() => console.log('done'))
   .catch(err => console.error(err))
   .finally(() => mongoose.connection.close());
 
-// seedData(100)
-//   .then(() => console.log('done'))
-//   .catch(err => console.error(err))
-//   .finally(() => mongoose.connection.close());
+pdxparent({})
+  .then(() => console.log('done'))
+  .catch(err => console.error(err))
+  .finally(() => mongoose.connection.close());
+
+library({})
+  .then(() => console.log('done'))
+  .catch(err => console.error(err))
+  .finally(() => mongoose.connection.close());
--- seedScraper.js
+++ seedScraper.js
@@ -1,6 +1,6 @@
 const eventScraper = require('./lib/services/saturdayAcademy');
 const library = require('./lib/services/libraryScraper');
-const pdxparent = require('./lib/services/pdxParent');
+// const pdxparent = require('./lib/services/pdxParent');
 
-pdxparent();
+// pdxparent();
 library();
