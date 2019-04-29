1bdaaa808ca9ad6db7c8b123f3c5456de0204248
Shaba9 <shaba92@gmail.com>
2019-04-28 21:44:13 -0700

    added to User model

--- tests/models/User.test.js
+++ tests/models/User.test.js
@@ -15,12 +15,30 @@ describe('User model', () => {
     return User.create({
       role: 'org',
       username: 'theOrg123',
-      password: 'passit'
+      password: 'passit',
+      name: 'The Org',
+      email: 'theorg@email.com',
+      phone: '555-123-4567',
+      address: {
+        street: '123 Main St.',
+        city: 'Portland',
+        state: 'OR',
+        zip: '97203'
+      }
     })
       .then(user => expect(user.toJSON()).toEqual({
         _id: expect.any(Object),
         role: 'org',
-        username: 'theOrg123'
+        username: 'theOrg123',
+        name: 'The Org',
+        email: 'theorg@email.com',
+        phone: '555-123-4567',
+        address: {
+          street: '123 Main St.',
+          city: 'Portland',
+          state: 'OR',
+          zip: '97203'
+        }
       }));
   });
 });
