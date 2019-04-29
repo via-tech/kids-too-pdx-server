2f21e511a58f71a3def6afa8bd8136731102df5b
Shaba9 <shaba92@gmail.com>
2019-04-29 11:43:19 -0700

    auth sign up route passes

---
+++ tests/routes/auth.test.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const connect = require('../../lib/utils/connect');
+const request = require('supertest');
+const app = require('../../lib/app');
+// const User = require('../../lib/models/User');
+
+describe('auth routes', () => {
+  // const createUser = username => User.create({
+  //   role: 'org',
+  //   username,
+  //   password: 'passit',
+  //   name: 'The Org',
+  //   email: 'theorg@email.com',
+  //   phone: '555-123-4567',
+  //   address: {
+  //     street: '123 Main St.',
+  //     city: 'Portland',
+  //     state: 'OR',
+  //     zip: '97203'
+  //   }
+  // })
+  //   .then(user => user.toJSON());
+
+  beforeAll(() => connect());
+
+  afterAll(done => {
+    mongoose.connection.dropDatabase();
+    mongoose.connection.close(done);
+  });
+
+  it('sings up an organization', () => {
+    return request(app)
+      .post('/auth/signup')
+      .send({
+        role: 'org',
+        username: 'org2',
+        password: 'passit2',
+        name: 'The Org2',
+        email: 'theorg2@email.com',
+        phone: '555-123-4568',
+        address: {
+          street: '124 Main St.',
+          city: 'Portland',
+          state: 'OR',
+          zip: '97203'
+        }
+      })
+      .then(res => expect(res.body).toEqual({
+        user: {
+          _id: expect.any(String),
+          role: 'org',
+          username: 'org2',
+          name: 'The Org2',
+          email: 'theorg2@email.com',
+          phone: '555-123-4568',
+          address: {
+            street: '124 Main St.',
+            city: 'Portland',
+            state: 'OR',
+            zip: '97203'
+          }
+        },
+        token: expect.any(String)
+      }));
+  });
+});
