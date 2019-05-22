d928d1a4ed56de924ae8259345a7eaf5af288c78
Shaba9 <shaba92@gmail.com>
2019-05-22 12:47:06 -0700

    tests fixed for liability

--- tests/routes/events.test.js
+++ tests/routes/events.test.js
@@ -38,6 +38,7 @@ describe('event routes', () => {
         ageMin: 2,
         ageMax: 14,
         price: 100,
+        liability: true,
         token: currentUser.token
       });
   };
@@ -68,6 +69,7 @@ describe('event routes', () => {
         ageMin: 2,
         ageMax: 14,
         price: 100,
+        liability: true,
         user: expect.any(String)
       }));
   });
@@ -99,6 +101,7 @@ describe('event routes', () => {
             ageMin: 2,
             ageMax: 14,
             price: 100,
+            liability: true,
             _id: expect.any(String)
           }));
       });
