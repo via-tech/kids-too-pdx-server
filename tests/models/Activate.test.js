09be593a70dd587d98f95bafa8acbe3666468b54
Shaba9 <shaba92@gmail.com>
2019-08-01 20:05:29 -0700

    test fix

--- tests/models/Activate.test.js
+++ tests/models/Activate.test.js
@@ -4,12 +4,12 @@ const Activate = require('../../lib/models/Activate');
 describe('Activate model', () => {
   it('creates an activation', () => {
     return Activate.create({
-      user: 'kids@email.com',
+      user: 'id123',
       code: '123abc' 
     })
       .then(activate => expect(activate.toJSON()).toEqual({
         _id: expect.any(Object),
-        user: 'kids@email.com',
+        user: 'id123',
         code: '123abc'
       }));
   });
